56e6ea66fcc453b27aad7e5a62837df69391f2aa
Haruki Matsukawa <sqwk7759@gmail.com>
2022-11-25 16:17:08 +0900

    product

---
+++ product.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3 -x
+#SPDX-FileCopyrightText: 2022 Haruki Matsukawa 
+#SPDX-License-Identifier: BSD-3-Clause
+import sys
+
+ans = 0
+def product(a,b):
+    ans = a * b
+    return ans
+
+x = product(3,4)
+
+print(x)
